45a1aa19e619b3918eefa44ab32918e0727f6faf
uday dosajh <dosajhuday@gmail.com>
2020-12-12 16:28:07 +0530

    chit fund

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { CallsComponent } from "./calls/calls.component";
 import { ChatsComponent } from "./chats/chats.component";
-import { StatusComponent } from "./status/status.component";
 import { FormsModule } from "@angular/forms";
 import { IonicModule } from "@ionic/angular";
 
 @NgModule({
-  declarations: [ChatsComponent, StatusComponent, CallsComponent],
+  declarations: [ChatsComponent],
   imports: [CommonModule, FormsModule, IonicModule],
-  exports: [ChatsComponent, StatusComponent, CallsComponent]
+  exports: [ChatsComponent]
 })
 export class ComponentsModule {}
