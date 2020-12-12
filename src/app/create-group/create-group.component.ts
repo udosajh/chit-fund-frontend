f426a7188f06078efa50db1ad4c99fea27f4519d
Vaibhav  Mishra <vaibhavmishra@Vaibhavs-MacBook-Air.local>
2020-12-13 00:21:56 +0530

    Adding more components

--- src/app/create-group/create-group.component.ts
+++ src/app/create-group/create-group.component.ts
@@ -6,9 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-group.component.scss'],
 })
 export class CreateGroupComponent implements OnInit {
+jsonData: any = [];
+  constructor() { 
+    this.initializeJsonData();
+  }
+ FilterJSONData(ev: any){
+   this.initializeJsonData();
+   const val = ev.target.value;
+   if(val && val.trim()!= ''){
+     this.jsonData = this.jsonData.filter((item) => {
+       return (item.name.toLowerCase().indexOf(val.toLowerCase())>-1);
+     })
+   }
+ }
 
-  constructor() { }
+  initializeJsonData(){
+    this.jsonData = [
+      {
+        "name": "Urvi",
+        "mobile": "123"
+      },
+      {
+        "name": "Shruti",
+        "mobile": "12334"
+      },
+      {
+        "name": "Kirti",
+        "mobile": "123222222"
+      },
+      {
+        "name": "Ila",
+        "mobile": "12453433"
+      }
+    ]
+  }
 
   ngOnInit() {}
+selectVal(val){
 
+}
 }
